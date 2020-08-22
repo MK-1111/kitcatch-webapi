8f3baa8f8efddf123ad4c2f2edfa7d177b367a4f
MK-1111 <mkkt2615@gmail.com>
2020-08-23 07:32:03 +0900

    fix two functions. then success

--- functions/index.js
+++ functions/index.js
@@ -6,9 +6,13 @@ const cors = require('cors');
 const app =express();
 app.use(cors({origin: true}));
 
+var serviceAccount = require("/home/star333/kitcatch/functions/serviceAccountKey.json");
+
 admin.initializeApp({
-    credential: admin.credential.applicationDefault()
-  });
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://friendlychat-1b9cf.firebaseio.com"
+});
+
 var db=admin.firestore();
 /*
 
@@ -19,36 +23,38 @@ app.get("/get_uuid/",(req,res,next)=>{
 app.get("/get_timetable/:userId",async (req,res,next)=>{
     try{
         var timetable_data=[];
-        db.collection("groups").doc(req.params.userId).collection(time-schedule).get().then(querySnapshot =>{
+        db.collection("groups").doc(req.params.userId).collection("time-schedule").get()
+        .then(querySnapshot =>{
             querySnapshot.forEach(doc =>{
                 timetable_data.push({
                     id:doc.id,
-                    data:doc.data()
+                    ...doc.data()
                 });
             });
         res.json(timetable_data);
         });
     }
-    catch(e){
-        next(e);
+    catch(error){
+        next(error);
     }
 });
 
 app.get("/get_task/:userId", async (req,res,next)=>{
     try{
         var task_data=[];
-        db.collection("groups").doc(req.params.userId).collection(homework).get().then(querySnapshot =>{
+        db.collection("groups").doc(req.params.userId).collection("homework").get()
+        .then(querySnapshot =>{
             querySnapshot.forEach(doc =>{
                 task_data.push({
                     id:doc.id,
-                    data:doc.data()
+                    ...doc.data()
                 });
             });
         res.json(task_data);
         });
     }
-    catch(e){
-        next(e);
+    catch(error){
+        next(error);
     }
 });
 
@@ -63,5 +69,5 @@ app.post("/post_task/:userId",(req,res,next)=>{
     res.send();
 });
 */
-const api=functions.https.onRequest(app);
+const api=functions.region("asia-northeast1").https.onRequest(app);
 module.exports={api};
