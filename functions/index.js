6cf9918e9a24edd39594aae38f63962b0fa7bb7f
MK-1111 <mkkt2615@gmail.com>
2020-08-22 14:04:01 +0900

    add two functions

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,41 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require('cors');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.get_timetable = functions.https.onRequest((request, response) => {
-  
+const app =express();
+app.use(cors({origin: true}));
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+  });
+var db=admin.firestore();
+/*
+
+app.get("/get_uuid/",(req,res,next)=>{
+    res.send();
+});
+*/
+app.get("/get_timetable/:userId",(req,res,next)=>{
+    const timetable=db.collection("groups").doc(req.params.userId).collection(time-schedule); 
+    res.json(timetable);
 });
 
-exports.get_task=functions.https.onRequest((request, response) => {
-  
+app.get("/get_task/:userId",(req,res,next)=>{
+    const task=db.collection("groups").doc(req.params.userId).collection(homework);  
+    res.send(task);
 });
 
-exports.get_uuid=functions.https.onRequest((request, response) => {
-  
+/*
+app.post("/post_timetable/:userId",(req,res,next)=>{
+    const 
+    res.send();
 });
 
-
-exports.echo = functions.https.onRequest((request, response) => {
-    const task = request.body
-    console.log(JSON.stringify(task))
-    // const firestore = admin.firestore()
-    // const ref = firestore.collection("todos")
-     response.send(JSON.stringify(task));
+app.post("/post_task/:userId",(req,res,next)=>{
+    const 
+    res.send();
 });
+*/
+const api=functions.https.onRequest(app);
+module.export={api};
